055ec1aeae4914e5cbf6ff27ea946cd757a3162c
FlanMan04 <clubgus2004@gmail.com>
2026-02-04 10:51:36 -0400

    Added Inventory.h; Polished Item.h

--- Inventory.h
+++ Inventory.h
@@ -1,8 +1,31 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
+#include <Item.h>
+#include <vector>
+
+using namespace std;
 
 // Implementa aquí la clase Inventory
 // Debe incluir el constructor Inventory(int maxSlots, int maxWeight)
 // Sigue la interfaz especificada en la asignación
+
+class Inventory {
+    private:
+        std::vector <Item> item;
+        int maxSlots;
+        int maxWeight;
+    public:
+        Inventory(int maxSlots, int maxWeight);
+
+        bool addItem(const Item& item);
+        bool removeItem(const std::string& name);
+        bool hasItem(const std::string& name) const;
+
+        int getCurrentWeight() const;
+        int getItemCount() const;
+        int getMaxSlots() const;
+        int getMaxWeight() const;
+}; 
+
 
 #endif
--- Item.h
+++ Item.h
@@ -11,19 +11,19 @@ using namespace std;
 enum ItemType {HEALING, WEAPON, ARMOR, MISC};
 
 class Item {
-private:
-    string name;
-    int weight;
-    int value;
-    ItemType type;
-    int power;
-public:
-    Item() {};
-    string getName ();
-    int getWeight ();
-    int getValue ();
-    int getPower ();
-    ItemType getType ();
+    private:
+        std::string name;
+        int weight;
+        int value;
+        ItemType type;
+        int power;
+    public:
+        Item(std::string name, int weight, int value, ItemType type, int power);        
+        string getName ();
+        int getWeight ();
+        int getValue ();
+        int getPower ();
+        ItemType getType ();
 };
 
 #endif
