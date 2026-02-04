f105533956a12a5ad191132133fa5896c13a9655
FlanMan04 <clubgus2004@gmail.com>
2026-02-04 11:51:24 -0400

    added functions for both .cpp files

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,3 +1,33 @@
 #include "Inventory.h"
+Inventory::Inventory(int maxSlots, int maxWeight) : maxSlots(maxSlots), maxWeight(maxWeight) {}
+Inventory::Inventory() {}
+Inventory::~Inventory() {}
 
+bool Inventory::addItem(const Item& item){
+
+}
+
+bool Inventory::removeItem(const std::string& name) {
+
+}
+
+bool Inventory::hasItem(const std::string& name) const {
+    
+}
+
+int Inventory::getCurrentWeight() const {
+
+}
+
+int Inventory::getCurrentWeight() const {
+    
+}
+
+int Inventory::getCurrentWeight() const {
+    
+}
+
+int Inventory::getCurrentWeight() const {
+    
+}
 // Implementa aquí los métodos de Inventory
--- Item.cpp
+++ Item.cpp
@@ -1,3 +1,31 @@
 #include "Item.h"
 
-// Implementa aquí los métodos de Item
+Item::Item(std::string name, int weight, int value, ItemType type, int power) : name(name), weight(weight), value(value), type(type), power(power) {}
+Item::Item() {}
+Item::~Item() {}
+
+std::string Item::getName() {
+    return name;
+}
+
+int Item::getWeight() {
+    if(weight < 0)
+        weight = 0;
+    return weight;
+}
+
+int Item::getValue() {
+    if (value < 0)
+        value = 0;
+    return value;
+}
+
+int Item::getPower() {
+    if (power < 0)
+        power = 0;
+    return power;
+}
+
+ItemType Item::getType() {
+    return type;
+}
